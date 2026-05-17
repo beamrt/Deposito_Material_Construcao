19fab1f090e3e2f2f58689ace5df1fcd64e3567c
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-05-17 20:32:35 -0300

    design da tela de login parcialmente finalizado

--- Deposito_Material_Construcao/frontend/src/pages/Login/LoginPage.jsx
+++ Deposito_Material_Construcao/frontend/src/pages/Login/LoginPage.jsx
@@ -1,12 +1,41 @@
 import * as log from './styled';
+import { FaGoogle } from 'react-icons/fa';
+import { FaFacebook } from 'react-icons/fa';
+import { FaApple } from 'react-icons/fa';
 
 export default function Login() {
   return (
     <log.Background>
+      <log.ContainerLogo />
+
       <log.TitleContainer>
         <log.Title>Olá, Seja bem-vindo(a)!</log.Title>
         <log.Subtitle>Faça login para continuar</log.Subtitle>
       </log.TitleContainer>
+
+      <log.ContainerForm>
+        <log.Form>
+          <log.InputEmail type="email" placeholder="E-mail" />
+          <log.InputSenha type="passoword" placeholder="Senha" />
+
+          <log.ButtonSubmit>Entrar</log.ButtonSubmit>
+        </log.Form>
+
+        <log.InformationContainer>
+          <log.Informations>
+            É novo por aqui? <span>Cadastre-se</span>
+          </log.Informations>
+          <log.Informations>
+            Esqueceu a sua senha? Clique <span>AQUI</span>
+          </log.Informations>
+        </log.InformationContainer>
+
+        <log.WrapperIcons>
+          <FaGoogle className="google" />
+          <FaApple className="apple" />
+          <FaFacebook className="face" />
+        </log.WrapperIcons>
+      </log.ContainerForm>
     </log.Background>
   );
 }
--- Deposito_Material_Construcao/frontend/src/pages/Login/styled.jsx
+++ Deposito_Material_Construcao/frontend/src/pages/Login/styled.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import backgroundSvg from '../../assets/images/background.svg';
+import logo from '../../assets/images/logoazul.png';
 
 export const Background = styled.div`
   background-image: url(${backgroundSvg});
@@ -8,6 +9,7 @@ export const Background = styled.div`
   background-size: cover;
   width: 100%;
   height: 100vh;
+  position: relative;
 `;
 
 export const TitleContainer = styled.div`
@@ -45,6 +47,155 @@ export const Subtitle = styled.h3`
   background: rgba(0, 0, 0, 0.7);
   width: 500px;
   border-radius: 20px;
-  border: 2px solid #252525;
+  border: 2px solid #df6c01;
   padding: 5px;
+`;
+
+export const ContainerLogo = styled.div`
+  position: absolute;
+  width: 250px;
+  height: 250px;
+  border-radius: 50%;
+  transform: translateX(-50%, -50%);
+  top: 37%;
+  left: 42.5%;
+  background-image: url(${logo});
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+export const ContainerForm = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  flex-direction: column;
+  width: 650px;
+  margin: 60px 30px;
+  padding: 10px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  flex-direction: column;
+  gap: 40px;
+  width: 100%;
+  height: 100%;
+  padding: 10px;
+`;
+
+export const InputEmail = styled.input`
+  width: 60%;
+  padding: 12px;
+  border: 2px solid #ccc;
+  background: #d9d9d9;
+  border-radius: 7px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  font-size: 15px;
+  transition: all 0.2s;
+
+  &:focus {
+    outline: none;
+    border-color: #df6c01;
+  }
+`;
+
+export const InputSenha = styled.input`
+  width: 60%;
+  padding: 12px;
+  border: 2px solid #ccc;
+  background: #d9d9d9;
+  border-radius: 7px;
+  font-size: 15px;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  transition: all 0.2s;
+
+  &:focus {
+    outline: none;
+    border-color: #df6c01;
+  }
+`;
+
+export const ButtonSubmit = styled.button`
+  padding: 10px;
+  width: 170px;
+  border: 1px solid #d9d9d9;
+  background: #0f766e;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 700;
+  font-style: normal;
+  color: #fff4e6;
+  border-radius: 8px;
+  font-size: 17px;
+  cursor: pointer;
+  transition: all 0.3s ease-in-out;
+  margin-left: 10px;
+  box-shadow: 6px 8px 12px rgba(0, 0, 0, 0.3);
+
+  &:hover {
+    transform: scale(1.05);
+    filter: brightness(90%);
+  }
+`;
+
+export const InformationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 60%;
+  height: 100px;
+  margin-left: 10px;
+  gap: 20px;
+  border-bottom: 1px solid #fff4e6;
+  margin-top: 20px;
+`;
+
+export const Informations = styled.p`
+  font-family: 'Poppins', sans-serif;
+  font-weight: 200;
+  font-style: normal;
+  color: #fff4e6;
+
+  span {
+    display: inline-block;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 500;
+    font-style: normal;
+    color: #df6c01;
+    cursor: pointer;
+    transition: all 0.3s ease-in;
+
+    &:hover {
+      transform: scale(1.06);
+    }
+  }
+`;
+
+export const WrapperIcons = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-start;
+  gap: 40px;
+  width: 60%;
+  padding: 20px;
+  margin-top: 20px;
+  margin-left: 10px;
+
+  .google,
+  .apple,
+  .face {
+    color: #fff4e6;
+    font-size: 30px;
+    transition: all 0.2s ease-in;
+    cursor: pointer;
+
+    &:hover {
+      transform: scale(1.08);
+    }
+  }
 `;
