d1c4c5729ab2a1394532f6a0e8861e119b5e71bd
1freelipe <feliperodrigues_spfc@hotmail.com>
2026-05-30 11:58:12 -0300

    feat: capturando todas as lojas e enviando em um formato JSON para o frontend

--- Deposito_Material_Construcao/backend/config/urls.py
+++ Deposito_Material_Construcao/backend/config/urls.py
@@ -22,17 +22,21 @@ from apps.usuarios.views import (
     api_alternar_unidade  
 )
 from apps.auditoria.views import api_auditoria
+from apps.lojas.views import api_lojas
 
 urlpatterns = [
+    # Sistema de autenticação (Cadastro, Login e Auditoria) 
     path('admin/', admin.site.urls),
     path('api/auth/login/', api_login, name='api_login'),
     path('api/auth/logout/', api_logout, name='api_logout'),
     path('api/auth/recuperar-senha/', api_recuperar_senha, name='api_recuperar_senha'),
     
-
     path('api/auth/alternar-unidade/', api_alternar_unidade, name='api_alternar_unidade'),
     
     path('api/usuarios/', api_usuarios, name='api_usuarios'),
     path('api/usuarios/<int:pk>/', api_usuario_detail, name='api_usuario_detail'),
     path('api/auditoria/', api_auditoria, name='api_auditoria'),
+
+    # Lojas
+    path('api/lojas/index', api_lojas, name='api_lojas')
 ]
